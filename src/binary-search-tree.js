05a3d45d1be654d2d9a63ff46178547cc0bfaaca
Andrei <platonovaudit@yandex.ru>
2022-10-13 15:17:01 +0500

    fix: BST pass has test

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,41 +6,130 @@ const { NotImplementedError } = require('../extensions/index.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
+  constructor() {
+    this.top = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.top;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    if (this.top === null) {
+      this.top = newNode;
+      return;
+    }
+    let temp = this.top
+    while (true) {
+      if (newNode.data < temp.data) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return;
+        } 
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return;
+        } 
+        temp = temp.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) { 
+    return this.find(data) !== null;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if (this.top === null) {
+      return null;
+    }
+    let temp = this.top;
+    while (temp.data !== data) { 
+      if (temp.data > data) {
+        if (temp.left === null) {
+          return null;
+        } else {
+          temp = temp.left;
+        }
+      } else {
+        if (temp.right === null) {
+          return null;
+        } else {
+          temp = temp.right;
+        }
+      }
+    }
+    return temp;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (this.top === null) {
+      return;
+    }
+    let temp = this.top;
+    if (temp.data === data) {
+      if (temp.left !== null && temp.right === null) {
+        this.top = this.top.left;
+        return;
+      }
+      if (temp.right !== null && temp.left === null) {
+        this.top = this.top.right;
+        return;
+      }
+       if (temp.right === null && temp.left === null) {
+        this.top = new Node;
+        return;
+      }
+      let newNode = this.top.right;
+  //    console.log(newNode);
+      while (newNode.left !== null) {
+        newNode = newNode.left;
+      }
+      newNode.left = temp.left;
+      newNode.right = temp.right;
+    }
+    while (temp.data !== data) { 
+      if (temp.data > data) {
+        if (temp.left === null) {
+          return;
+        } else {
+          temp = temp.left;
+        }
+      } else {
+        if (temp.right === null) {
+          return;
+        } else {
+          temp = temp.right;
+        }
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.top === null) {
+      return null;
+    }
+    let temp = this.top;
+    while (temp.left !== null) {
+      temp = temp.left;
+    }
+    return temp.data ?? null;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.top === null) {
+      return null;
+    }
+    let temp = this.top;
+    while (temp.right !== null) {
+      temp = temp.right;
+    }
+    return temp.data ?? null;
   }
 }
 
