1dba93961ea89bbb0898278c7ea403c3e82ba431
Andrei <platonovaudit@yandex.ru>
2022-10-13 09:23:55 +0500

    fix: stack pass test

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.arr = new Array;
+  }
+
+  push(element) {
+    this.arr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let size = this.arr.length;
+    let last;
+    if (size > 0) {
+      last = this.arr[size - 1];
+      this.arr = this.arr.slice(0, size -1);
+    }
+    return last;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.arr.length > 0) {
+      return this.arr[this.arr.length - 1]
+    }
   }
 }
 
